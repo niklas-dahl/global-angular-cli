ce2fc8ef00d2aef7f8b2e489ba9a6bcd5a1a796d
Niklas Dahl <niklas.dahl@inform-software.com>
2016-11-06 14:11:00 +0100

    proxy early to angular-cli WIP

--- src/proxyCommand.js
+++ src/proxyCommand.js
@@ -2,40 +2,36 @@
 
 // pretty much taken from https://github.com/angular/angular-cli/blob/master/packages/angular-cli/bin/ng
 
-const resolve = require('resolve');
-const exit = require('exit');
+const resolvePackage = require('resolve');
 
-function proxyCommand() {
-  resolve('angular-cli', { basedir: process.cwd() },
-  
-  function (error, projectLocalCli) {
-    if (error) {
-      // If there is an error, resolve could not find the ng-cli
-      // library from a package.json. Instead, include it from a relative
-      // path to this script file (which is likely a globally installed
-      // npm package). Most common cause for hitting this is `ng new`
-      console.log('ERR', 'Local angular-cli could not be found. Make sure to install your depdencies first');
-      exit(1);
-    } else {
-      // No error implies a projectLocalCli, which will load whatever
-      // version of ng-cli you have installed in a local package.json
-      let cli = require(projectLocalCli);
-     
-      if ('default' in cli) {
-        cli = cli['default'];
+function proxyCommand(args) {
+  return new Promise((resolve, reject) => {
+
+    resolvePackage('angular-cli', { basedir: process.cwd() }, function (error, projectLocalCli) {
+      if (error) {
+        // If there is an error, resolve could not find the ng-cli
+        // library from a package.json. Instead, include it from a relative
+        // path to this script file (which is likely a globally installed
+        // npm package). Most common cause for hitting this is `ng new`
+        console.log('ERR', 'Local angular-cli could not be found. Make sure to install your depdencies first');
+        reject(error);
+      } else {
+        // No error implies a projectLocalCli, which will load whatever
+        // version of ng-cli you have installed in a local package.json
+        let cli = require(projectLocalCli);
+      
+        if ('default' in cli) {
+          cli = cli['default'];
+        }
+
+        return cli({
+          cliArgs: args,
+          inputStream: process.stdin,
+          outputStream: process.stdout,
+          // Leek: CustomLeek
+        });
       }
-
-      cli({
-        cliArgs: process.argv.slice(2),
-        inputStream: process.stdin,
-        outputStream: process.stdout,
-        // Leek: CustomLeek
-      }).then(function (result) {
-        var exitCode = typeof result === 'object' ? result.exitCode : result;
-        exit(exitCode);
-      });
-    }
-
+    });
   });
 }
 
